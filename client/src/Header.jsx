4a92a5604e1c6a6b960c8f4b4b90a4b54dea5b65
Clément Poudrée <markatio77@gmail.com>
2023-03-21 17:22:10 +0100

    Fixed mobile responsive

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -22,11 +22,11 @@ const Header = () => {
             d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
           />
         </svg>
-        <span className="font-bold text-xl">airbnb</span>
+        <span className="hidden sm:block font-bold text-xl">airbnb</span>
       </Link>
 
       {/* Mid */}
-      <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-3 shadow-md shadow-gray-300">
+      <div className="hidden sm:flex gap-2 border border-gray-300 rounded-full py-2 px-3 shadow-md shadow-gray-300">
         <div>Anywhere</div>
         <div className="border-l border-gray-300"></div>
         <div>Any week</div>
--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -4,7 +4,7 @@ import Header from './Header'
 
 const Layout = () => {
   return (
-    <div className='max-w-7xl mx-auto flex flex-col min-h-screen py-4 px-8'>
+    <div className='sm:max-w-7xl mx-auto flex flex-col min-h-screen py-4 px-4 sm:px-8'>
         <Header />
         <Outlet />
     </div>
