ec005fe737b9964a14d57350cd23ed2261200d79
Clément Poudrée <markatio77@gmail.com>
2023-03-21 17:29:01 +0100

    Fixed width of Account Navigation for mobile

--- client/src/AccountNavigation.jsx
+++ client/src/AccountNavigation.jsx
@@ -7,7 +7,7 @@ export default function AccountNav() {
         subpage = "profile";
     }
     function linkClasses(type=null){
-        let classes = 'rounded-full inline-flex gap-1 items-center py-2 px-6';
+        let classes = 'rounded-full flex gap-1 justify-center items-center py-2 px-6';
 
         if(type === subpage){
             classes += ' bg-primary text-white';
@@ -19,7 +19,7 @@ export default function AccountNav() {
     }
 
   return (
-    <nav className="w-full flex mt-8 justify-center gap-2 mb-8">
+    <nav className="w-full flex flex-col sm:flex-row mt-8 justify-center gap-2 mb-8">
         <Link className={linkClasses('profile')} to={'/account'}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
