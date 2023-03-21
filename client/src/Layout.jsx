74a2f5a27e5be66e450d922d2b0d5c3bf8648dfd
Clément Poudrée <markatio77@gmail.com>
2023-03-21 17:10:38 +0100

    Added space to the sides of the pages layout

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -4,7 +4,7 @@ import Header from './Header'
 
 const Layout = () => {
   return (
-    <div className='flex flex-col min-h-screen py-4 px-8'>
+    <div className='max-w-7xl mx-auto flex flex-col min-h-screen py-4 px-8'>
         <Header />
         <Outlet />
     </div>
